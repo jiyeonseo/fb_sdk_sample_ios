5b76d704904793dae53f40239eb74d0ff554c00b
Jiyeon Seo <jiyeonseo@fb.com>
2018-04-11 17:57:29 +0900

    Initial Commit

---
+++ FbSDKSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FbSDKSample
+//
+//  Created by Jiyeon Seo on 4/11/18.
+//  Copyright © 2018 Jiyeon Seo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FbSDKSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FbSDKSample
+//
+//  Created by Jiyeon Seo on 4/11/18.
+//  Copyright © 2018 Jiyeon Seo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
